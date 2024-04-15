c9fb0bead023188972a13758ba19f77cc4aa0f55
ranju58 <158138118+ranju58@users.noreply.github.com>
2024-04-15 20:10:08 +0545

    Update edit-articles.php

--- article-mgmt/edit-articles.php
+++ article-mgmt/edit-articles.php
@@ -1,5 +1,6 @@
 <?php
 include("connection.php");
+$id   = $_GET['id'];
 
 if (isset($_POST['Edit'])) {
     $id   = $_GET['id'];
@@ -7,14 +8,17 @@ if (isset($_POST['Edit'])) {
     $Content = $_POST['content'];
     $Image = file_get_contents($_FILES['image']['tmp_name']);
 
+
     $sql  = "UPDATE articles SET title='$Title', content='$Content',  image='0x" . bin2hex($Image) . "' WHERE id='$id'";
     //echo '<br>'.$sql;
 
     $res = mysqli_query($conn, $sql);
-    // or die("Could not update".mysqli_error());  
-    header('location:dashboard.php');
+    // header('location:dashboard.php');
     mysqli_close($conn);
 }
+$sql = "SELECT * FROM articles WHERE id='$id'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,7 +26,7 @@ if (isset($_POST['Edit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Articles</title>
+    <title>Edit Article</title>
     <link rel="stylesheet" href="register.css" />
 
 </head>
@@ -34,11 +38,12 @@ if (isset($_POST['Edit'])) {
         <form class="add-article" action="#" method="POST" enctype="multipart/form-data">
             <div class="fields">
                 <label for="title">Title</label>
-                <input type="text" name="title" id="title" placeholder="title" required /><br />
+                <input type="text" name="title" id="title" placeholder="title" value="<?= $row['title'] ?>" /><br />
                 <label for="content">Content</label>
-                <input type="text" name="content" id="content" placeholder="content" required /><br />
+                <input type="text" name="content" id="content" placeholder="content" value="<?= $row['content'] ?>" /><br />
                 <label for="content">Image</label>
-                <input type="file" name="image" id="image" placeholder="choose file" accept="image/*" required /><br />
+                <img src="data:image/jpeg;base64,<?php echo base64_encode($row['image']); ?>">
+                <input type="file" name="image" id="image" placeholder="choose file" accept="image/*" /><br />
                 <button type="Submit" name="Edit">Edit Article</button><br />
             </div>
         </form>
