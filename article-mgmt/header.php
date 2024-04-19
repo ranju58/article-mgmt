706b19f102943df1a5d59447ccd80cb011c3f697
ranju58 <158138118+ranju58@users.noreply.github.com>
2024-04-19 11:09:45 +0545

    Create header

---
+++ article-mgmt/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title> form</title>
+    <link rel="stylesheet" href="register.css" />
+</head>
